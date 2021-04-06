15e691af0770c83f1532ce3b2cac3c0724fe3a52
Kara <palumbokara@yahoo.com>
2021-04-06 15:42:36 -0600

    Adding rest of node-files project

--- step3.js
+++ step3.js
@@ -2,30 +2,52 @@ const fs = require("fs");
 const process = require("process");
 const axios = require("axios");
 
-function cat(path) {
-  fs.readFile(path, "utf8", (err, data) => {
+function output(text, out) {
+  if (out) {
+    fs.writeFile(out, text, "utf8", function (err) {
+      if (err) {
+        console.error(`Couldn't write ${out}: ${err}`);
+        process.exit(1);
+      }
+    });
+  } else {
+    console.log(text);
+  }
+}
+
+function cat(path, out) {
+  fs.readFile(path, "utf8", function (err, data) {
     if (err) {
-      console.log(`Error reading ${path}: ${err}`);
+      console.error(`Error reading ${path}: ${err}`);
       process.exit(1);
+    } else {
+      output(data, out);
     }
-    console.log(data);
   });
 }
 
-async function webCat(url) {
+async function webCat(url, out) {
   try {
     let res = await axios.get(url);
-    console.log(res.data);
+    output(res.data, out);
   } catch (err) {
-    console.log(`Error fetching ${url}: ${err}`);
+    console.error(`Error fetching ${url}: ${err}`);
     process.exit(1);
   }
 }
 
-let path = process.argv[2];
+let path;
+let out;
+
+if (process.argv[2] === "--out") {
+  out = process.argv[3];
+  path = process.argv[4];
+} else {
+  path = process.argv[2];
+}
 
 if (path.slice(0, 4) === "http") {
-  webCat(path);
+  webCat(path, out);
 } else {
-  cat(path);
+  cat(path, out);
 }
